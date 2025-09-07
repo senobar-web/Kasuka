4ddae4def9cf5fbf2d726bf97187925ec66d43be
asal <assal.senobarnya@gmail.com>
2025-09-07 18:15:41 +0330

    create content file from content template

---
+++ src/components/template/Content/Content.jsx
@@ -0,0 +1,23 @@
+import About from "./About";
+import Banner from "./Banner";
+import Client from "./Client";
+import ContactUs from "./ContactUs";
+import ExampleWork from "./ExampleWork";
+import Features from "./Features";
+import Teams from "./Teams";
+import Testimonial from "./Testimonial";
+
+export default function Content() {
+  return (
+    <div>
+      <Banner />
+      <About />
+      <Client />
+      <Features />
+      <ExampleWork />
+      <Testimonial />
+      <Teams />
+      <ContactUs />
+    </div>
+  );
+}
