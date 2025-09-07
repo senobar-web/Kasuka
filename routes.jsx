e5495cf9cbab90da398593934b0cd9eebaa95bd5
asal <assal.senobarnya@gmail.com>
2025-09-07 17:37:57 +0330

    add routes to project

---
+++ routes.jsx
@@ -0,0 +1,7 @@
+import Index from "./src/components/pages/Index.jsx/Index"
+import Detailes from "./src/components/pages/detailes/detailes"
+let routes =[
+    {path:'/' , element:<Index />},
+    {path:'/detail' , element:<Detailes />},
+]
+export default routes
