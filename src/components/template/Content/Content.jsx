abcbb4b8472ae6a7dd78e42edb595eb753ac7349
asal <assal.senobarnya@gmail.com>
2025-09-29 12:05:15 +0330

    add fragment to content component

--- src/components/template/Content/Content.jsx
+++ src/components/template/Content/Content.jsx
@@ -9,7 +9,7 @@ import Testimonial from "./Testimonial";
 
 export default function Content() {
   return (
-    <div>
+    <>
       <Banner />
       <About />
       <Client />
@@ -18,6 +18,6 @@ export default function Content() {
       <Testimonial />
       <Teams />
       <ContactUs />
-    </div>
+    </>
   );
 }
