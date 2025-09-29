ab30ad4fed55beda3029b5a696e68930484c6fa3
asal <assal.senobarnya@gmail.com>
2025-09-29 12:21:49 +0330

    add fragment to scrollButton component

--- src/components/module/scrollButton/Scroll.jsx
+++ src/components/module/scrollButton/Scroll.jsx
@@ -24,7 +24,7 @@ export default function ScrollTop() {
   }, []);
 
   return (
-    <div>
+    <>
       {isVisible && (
         <button
           className="px-2.5 py-2.5 bg-[#ffd584] fixed bottom-6 right-4 z-50 rounded-md flex justify-center items-center group hover:bg-[#151515] "
@@ -33,6 +33,6 @@ export default function ScrollTop() {
           <FaArrowUp className="group-hover:text-[#ffd584] size-4" />
         </button>
       )}
-    </div>
+    </>
   );
 }
